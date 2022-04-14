8c98df43fe9ba26a78c5a30003b7dca13031af02
blarso236 <94015436+blarso236@users.noreply.github.com>
2022-04-14 13:52:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+function redirectA() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/svg/svg.html")
+}
+
+function redirectB() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/2D%20Arrays/index.html")
+}
+
+function redirectC() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Bootstrap/bootstrap.html")
+}
+
+function redirectD() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/clock/index.html")
+}
+
+function redirectE() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Constructor%20Function/index.html")
+}
+
+function redirectF() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/DOM/c05/index.html")
+}
+
+function redirectG() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/functions/index.html")
+}
+
+function redirectH() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Jquery/jquery.html")
+}
+
+function redirectI() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/JSON/locations.html")
+}
+
+function redirectJ() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/National%20Holidays/nationalHolidays.html")
+}
+
+function redirectK() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Transition%20Mail/Transition-Mail.html")
+}
+
+function redirectL() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Audio/audio.html")
+}
+
+function redirectM() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Audio/audio.html")
+}
+
+function redirectN() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Transitions/Index(1).html")
+}
+
+function redirectO() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Quiz/index.html")
+}
+
+function redirectTwo() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/Sem2.html#semtwo")
+}
+
+function redirectOne() {
+    window.location.replace("file:///C:/Users/blarso236/Documents/Portfolio/portfolio.html")
+}
